0b5f2bbc85a64458ad3796e56400ecc5475c6bdf
abhay phougat <abhay@cosmoneural.com>
2020-12-28 14:01:46 +0530

    try, catch block and also the conditional check on the string before trimming

--- src/jsontraformer.groovy
+++ src/jsontraformer.groovy
@@ -1,9 +1,10 @@
 import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
-import org.apache.nifi.processor.io.StreamCallback
 import org.apache.commons.io.IOUtils
+import org.apache.commons.lang3.StringUtils
+import org.apache.nifi.processor.io.StreamCallback
+
 import java.nio.charset.StandardCharsets
-import java.text.SimpleDateFormat
 
 def flowFile = session.get()
 if(!flowFile) return
@@ -15,78 +16,70 @@ flowFile = session.write(flowFile, { inputStream, outputStream ->
     String priorityList = jsonPayload.priorityList;
     def storePriority = jsonPayload.storePriority;
 
-    //priorityList.replaceAll("\r?\n", "")
-    //log.warn(storePriority.toString())
-    def trimmedObject = priorityList.substring(priorityList.indexOf("{{")+2, priorityList.indexOf("}}]"));
-    //log.warn(trimmedObject)
+    if(StringUtils.isNoneEmpty(priorityList) && priorityList.contains("{{") && priorityList.contains("}}]"))
+    {
+        def trimmedObject = priorityList.substring(priorityList.indexOf("{{")+2, priorityList.indexOf("}}]"));
 
 
-    List<String> values = trimmedObject.split(",\\s")
+        List<String> values = trimmedObject.split(",\\s")
 
-    values.each {String it ->
-        def keyVal = it.split("=")
-        //StringBuilder builder = new StringBuilder(keyVal[0]).append(":").append(keyVal[1])
-        def value = keyVal[1].toString()
-        def key = keyVal[0].toString();
+        values.each { String it ->
+            try {
+                def keyVal = it.split("=")
+                def value = keyVal[1].toString()
+                def key = keyVal[0].toString();
 
-        if(value.contains(","))
-        {
-            value = value.split(",")
-        }
-
-        if(key.equalsIgnoreCase("Classification"))
-        {
-            jsonPayload.productClassification = value;
-        }else if(key.equalsIgnoreCase("Fullfillment"))
-        {
-            jsonPayload.fullfillment = value;
-        }
-        else if(key.equalsIgnoreCase("returnStore") && value != '')
-        {
-            List obj = new ArrayList();
-            storePriority.each { store ->
-                if(store.name.equalsIgnoreCase(value))
-                {
-                    obj.add(store)
+                if (value.contains(",")) {
+                    value = value.split(",")
                 }
-            }
-            jsonPayload.returnStorePriority = obj;
-        }else if(key.equalsIgnoreCase("newPincode"))
-        {
-            jsonPayload.newPincode = value;
-        }
-        else if(key.equalsIgnoreCase("newCity"))
-        {
-            jsonPayload.newCity = value;
-        }
-        else if(key.equalsIgnoreCase("sourcingChange"))
-        {
-            jsonPayload.sourcingChange = value;
-        }
-        else if(key.equalsIgnoreCase("returnStoreChange"))
-        {
-            jsonPayload.returnStoreChange = value;
-        }
-        else if(key.contains("storePriority"))
-        {
-            storePriority.each { store ->
-                store.priority = 1000;
-                value.eachWithIndex {dcName, number ->
-                if(store.name.equalsIgnoreCase(dcName.toString()))
-                {
-                    store.priority = number
+
+                if (key.equalsIgnoreCase("Classification")) {
+                    jsonPayload.productClassification = value;
+                } else if (key.equalsIgnoreCase("Fullfillment")) {
+                    jsonPayload.fullfillment = value;
+                } else if (key.equalsIgnoreCase("returnStore") && value != '') {
+                    List obj = new ArrayList();
+                    storePriority.each { store ->
+                        if (store.name.equalsIgnoreCase(value)) {
+                            obj.add(store)
+                        }
+                    }
+                    jsonPayload.returnStorePriority = obj;
+                } else if (key.equalsIgnoreCase("newPincode")) {
+                    jsonPayload.newPincode = value;
+                } else if (key.equalsIgnoreCase("newCity")) {
+                    jsonPayload.newCity = value;
+                } else if (key.equalsIgnoreCase("sourcingChange")) {
+                    jsonPayload.sourcingChange = value;
+                } else if (key.equalsIgnoreCase("returnStoreChange")) {
+                    jsonPayload.returnStoreChange = value;
+                } else if (key.contains("storePriority")) {
+                    storePriority.each { store ->
+                        store.priority = 1000;
+                        value.eachWithIndex { dcName, number ->
+                            if (store.name.equalsIgnoreCase(dcName.toString())) {
+                                store.priority = number
+                            }
+                        }
+                    }
                 }
+                StringBuilder _id = new StringBuilder(jsonPayload.programId).append("_").append(jsonPayload.destnPincode).append("_").append(jsonPayload.productClassification)
+                jsonPayload._id = _id.toString()
+                jsonPayload.remove("priorityList")
+                //objectMap.put(key, value);
             }
+            catch (e) {
+                log.error("Error during processing", e)
+                //session.transfer(inputStream, REL_FAILURE)
             }
         }
-        StringBuilder _id = new StringBuilder(jsonPayload.programId).append("_").append(jsonPayload.destnPincode).append("_").append(jsonPayload.productClassification)
-        jsonPayload._id = _id.toString()
-        jsonPayload.remove("priorityList")
-        //objectMap.put(key, value);
+
+
+
+        def outJson = new JsonBuilder(jsonPayload);
+        outputStream.write(outJson.toString().getBytes(StandardCharsets.UTF_8))
     }
 
-    def outJson =  new JsonBuilder( jsonPayload );
-    outputStream.write(outJson.toString().getBytes(StandardCharsets.UTF_8))
 } as StreamCallback)
 session.transfer(flowFile, REL_SUCCESS)
 
